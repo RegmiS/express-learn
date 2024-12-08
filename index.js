f40ef92f1eaa8c9102890f9f6d1f27d2e378c6cf
RegmiS <sandeshreg35@gmail.com>
2024-12-08 15:01:32 -0600

    Init express setup

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+const port = 8002;
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
